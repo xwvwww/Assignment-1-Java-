e0da862bf4de4151f1419e10576359c6ad28cff2
Alnur (アルヌール) <82401346+xwvwww@users.noreply.github.com>
2023-12-18 11:09:54 +0600

    Add files via upload

---
+++ java/src/Main.java
@@ -0,0 +1,147 @@
+// Task.1
+
+//public class Main {
+//    private String name;
+//    private int age;
+//
+//    public Main(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public void displayInfo() {
+//        System.out.println("Person Information: Game Developer");
+//        System.out.println("Name: " + name);
+//        System.out.println("Age: " + age);
+//    }
+//
+//    public static void main(String[] args) {
+//        Main person = new Main("Alnur", 18);
+//
+//        person.displayInfo();
+//    }
+//
+//}
+
+
+
+// Task.2
+
+//public class Main {
+//    private String name;
+//    private int age;
+//
+//    public Main(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public void displayInfo() {
+//        System.out.println("Person Information: Game Developer");
+//        System.out.println("Name: " + name);
+//        System.out.println("Age: " + age);
+//    }
+//
+//    public void printDetails() {
+//        System.out.println("Person Details: Polyglot");
+//        System.out.println("Name: " + name);
+//        System.out.println("Age: " + age);
+//    }
+//
+//    public static void main(String[] args) {
+//        Main person = new Main("Alnur", 18);
+//        person.displayInfo();
+//        person.printDetails();
+//    }
+//}
+
+
+// Task.3
+
+//public class Main {
+//    private String name;
+//    private int age;
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void displayInfo() {
+//        System.out.println("Person Information: Game Developer");
+//        System.out.println("Name: " + getName());
+//        System.out.println("Age: " + getAge());
+//    }
+//
+//    public void printDetails() {
+//        System.out.println("Person Details: Polyglot");
+//        System.out.println("Name: " + getName());
+//        System.out.println("Age: " + getAge());
+//    }
+//
+//    public static void main(String[] args) {
+//        Main person = new Main();
+//        person.setName("Alnur");
+//        person.setAge(18);
+//
+//        person.displayInfo();
+//        person.printDetails();
+//    }
+//}
+
+
+// Task.4
+
+//public class Main {
+//    private String name;
+//    private int age;
+//
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void displayInfo() {
+//        System.out.println("Person Information: Game Developer");
+//        System.out.println("Name: " + getName());
+//        System.out.println("Age: " + getAge());
+//    }
+//
+//    public void printDetails() {
+//        System.out.println("Person Details: Polyglot");
+//        System.out.println("Name: " + getName());
+//        System.out.println("Age: " + getAge());
+//    }
+//
+//    public static void main(String[] args) {
+//        Main person = new Main();
+//        person.setName("Alnur");
+//        person.setAge(18);
+//        person.printDetails();
+//    }
+//}
+
+
